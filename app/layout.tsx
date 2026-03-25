eec30cb4e7d7b28259bf235aa0c298c64eaaef77
Miss-kinz <mehak313naqvi@gmail.com>
2026-03-25 19:05:42 +0500

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/molecules/Navbar";
 
 const inter = Inter({
   variable: "--font-inter-sans",
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
--- components/molecules/Navbar.jsx
+++ components/molecules/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-200 shadow-blue-500 shadow-md h- w-full'> </div>
-  )
-}
+    <div className="shadow-primary/40 shadow-md h-24 p-4 w-full">
+      <div className=" flex justify-between mx-auto items-start h-full max-w-7xl">
+        <Image src="/c-logo.jpg" alt="Calendra Logo" width={70} height={80} />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
